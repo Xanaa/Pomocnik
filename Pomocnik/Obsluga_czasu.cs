051b7547b7dff751c04a56303f6006c67e70e862
Xanaa <lary315@wp.pl>
2017-02-10 23:08:32 +0100

    0.1.0.0
    -Dodanie funkcji:
    a.Obsluga_czasu->Uspij
    b.Obsluga_czasu->Podaj_czas

---
+++ Pomocnik/Obsluga_czasu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Pomocnik
+{
+    public class Obsluga_czasu
+    {
+        // Uśpienie programu na x sekund
+        public static bool Uspij(int sekundy)
+        {
+            Thread.Sleep(sekundy * 1000); // Thread.Sleep(X) <--- czekaj Xms, więc 1000ms=1s
+            return true;
+        }
+
+        // Zwraca obecny czas - godzina:minuta:sekunda
+        public static string Podaj_czas()
+        {
+            return DateTime.Now.ToString("HH:mm:ss");
+        }
+    }
+}
---
+++ Pomocnik/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pomocnik")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pomocnik")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("96ff33f0-e81e-4535-aa75-a06a057524dc")]
+
+// Version information for an assembly:
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
