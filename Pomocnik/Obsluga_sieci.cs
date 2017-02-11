320591a02288f6b190ed65a4b10b39a8cae8ac12
Xanaa <lary315@wp.pl>
2017-02-11 01:37:48 +0100

    0.2.0.0
    -Dodanie funkcji:
    a. Obsluga_sieci->Wyszukaj_na_stronie

---
+++ Pomocnik/Obsluga_sieci.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+
+namespace Pomocnik
+{
+    public class Obsluga_sieci
+    {
+        // Wyszukaj linie, która zawiera "szukane1" pod adresem "strona" i usuń z tej linii "szukane1" i "szukane2"
+        public static string Wyszukaj_na_stronie(string strona, string szukane1, string szukane2)
+        {
+            string pobrana_strona = new WebClient().DownloadString(strona);
+            string do_wyswietlenia = "Brak szukanej zawartości na stronie!";
+
+            string[] przerobiona_strona = pobrana_strona.Split('\n');
+
+            foreach (string linia in przerobiona_strona)
+            {
+                if (linia.Contains(szukane1))
+                {
+                    do_wyswietlenia = linia.Replace(szukane1, "");
+                    do_wyswietlenia = do_wyswietlenia.Replace(szukane2, "");
+                    break;
+                }
+            }
+            return do_wyswietlenia;
+        }
+    }
+}
