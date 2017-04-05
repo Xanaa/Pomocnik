1646f8b824f48e881f8f85b7338016e64b173432
Xanaa <lary315@wp.pl>
2017-04-05 20:56:45 +0200

    1.3.0.1
    -Naprawiono błąd uniemożliwiający użycie funkcji z "Obsluga_tekstu"

--- Pomocnik/Obsluga_tekstu.cs
+++ Pomocnik/Obsluga_tekstu.cs
@@ -1,6 +1,6 @@
 ﻿namespace Pomocnik
 {
-    class Obsluga_tekstu
+    public class Obsluga_tekstu
     {
         // Znajdz w "tekst" i zamień "szukany" na "zamiennik"
         public static string Znajdz_i_zamien(string tekst, string szukany, string zamiennik)
