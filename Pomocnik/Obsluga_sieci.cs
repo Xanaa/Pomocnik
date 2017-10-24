3fd37e0efd032e993e26ec3a24af878704acfc97
XanaPL <lary315@wp.pl>
2017-10-24 17:46:52 +0200

    1.3.5.1

--- Pomocnik/Obsluga_sieci.cs
+++ Pomocnik/Obsluga_sieci.cs
@@ -4,22 +4,13 @@ namespace Pomocnik
 {
     public class Obsluga_sieci
     {
-        // Wyszukaj linie, która zawiera "szukane1" pod adresem "strona" i usuń z tej linii "szukane1" i "szukane2"
-        public static string Wyszukaj_na_stronie(string strona, string szukane1, string szukane2)
+        // Zwraca tekst ze strony na lini 0
+        public static string Podaj_linie_zero(string strona)
         {
             string pobrana_strona = new WebClient().DownloadString(strona);
             string do_wyswietlenia = "Brak szukanej zawartości na stronie!";
-            string[] przerobiona_strona = pobrana_strona.Split('\n');
+            do_wyswietlenia = pobrana_strona;
 
-            foreach (string linia in przerobiona_strona)
-            {
-                if (linia.Contains(szukane1))
-                {
-                    do_wyswietlenia = linia.Replace(szukane1, "");
-                    do_wyswietlenia = do_wyswietlenia.Replace(szukane2, "");
-                    break;
-                }
-            }
             return do_wyswietlenia;
         }
 
